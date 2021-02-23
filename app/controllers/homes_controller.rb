767ea38e0b0c06a298a3c5f6d51f462b604a585e
mi-1109 <meisuzuki.sophia@gmail.com>
2021-02-23 14:32:46 +0000

    [Add] 投稿機能：新規投稿の保存処理を追加（createアクションとストロングパラメータ)

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,14 +1,27 @@
 class HomesController < ApplicationController
-  
+
   def collection
   end
-  
+
   def index
+    @book = Book.new
   end
 
   def show
   end
 
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to homes_path
+  end
+
   def edit
   end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
