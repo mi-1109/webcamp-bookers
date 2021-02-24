1c44094555752ac0bdd5309520d37daf3afdaab7
mi-1109 <meisuzuki.sophia@gmail.com>
2021-02-24 02:08:53 +0000

    [Update]Changed controller name from homes to books

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :homes
+  resources :books
 
-  get 'collection' => 'homes#collection'
+  get 'collection' => 'books#collection'
   #アプリケーションの"/"ルートにアクセスしたとき、homesコントローラのcollectionアクションが呼ばれる
-  root "homes#collection"
+  root "books#collection"
 
-  post 'homes' => 'homes#create'
+  post 'books' => 'books#create'
 end
