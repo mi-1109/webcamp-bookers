77d347f489f3ea60040a515d9aa19d4aebb715c2
mi-1109 <meisuzuki.sophia@gmail.com>
2021-03-12 01:26:30 +0000

    [Fix] Route setting

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :books
-
-  get 'collection' => 'books#collection'
-  #アプリケーションの"/"ルートにアクセスしたとき、homesコントローラのcollectionアクションが呼ばれる
-  root "books#collection"
-
-  post 'books' => 'books#create'
+  root to: "books#collection"
 
   patch 'books/:id/edit' => 'books#update', as: 'update_book'
   delete 'books/:id' => 'books#destroy', as: 'destroy_book'
